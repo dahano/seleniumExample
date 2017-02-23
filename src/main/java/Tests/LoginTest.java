4993327a82e91f98760f39205825efb4dd36680b
ofir dahan <ofirdahan@live.com>
2017-02-22 20:54:33 -0800

    extended TestSetup to LoginTest

--- src/main/java/Tests/LoginTest.java
+++ src/main/java/Tests/LoginTest.java
@@ -1,63 +1,49 @@
 package Tests;
 
+import CommonObjects.TestSetup;
 import Pages.LoginPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 /**
  * Created by ofirdahan on 2/20/17.
  */
-public class LoginTest {
-    WebDriver loginDriver;
+public class LoginTest extends TestSetup {
     LoginPage loginObj;
 
     String invalidLoginMessage = "Either your login email or password is incorrect, please try again";
 
-    @BeforeTest
-    public void setup(){
-        String path = "/Applications/Firefox.app/Contents/MacOS/geckodriver";
-        System.setProperty("webdriver.gecko.driver", path);
-        loginDriver = new FirefoxDriver();
-        loginDriver.get("https://www.headspace.com/");
-    }
-
     @Test
     public void validateLoginUrlSet() throws InterruptedException {
-        loginObj = new LoginPage(loginDriver);
+        loginObj = new LoginPage(driver);
         loginObj.clickLoginLink();
         Thread.sleep(2000);
         loginObj.validateLoginUrlcontains("login");
-        Assert.assertTrue(loginDriver.findElement(By.xpath("//html/body/div[1]/section/div/h1")).getText().contains("LOGIN"));
+        Assert.assertTrue(driver.findElement(By.xpath("//html/body/div[1]/section/div/h1")).getText().contains("LOGIN"));
     }
 
     @Test
     public void validateSignUpLinkUrl() throws InterruptedException {
-        loginObj = new LoginPage(loginDriver);
+        loginObj = new LoginPage(driver);
         loginObj.clickSignUpLink();
         Thread.sleep(2000);
         loginObj.validateLoginUrlcontains("register");
-        Assert.assertTrue(loginDriver.findElement(By.xpath("//html/body/div[1]/section[2]/div[1]/h2")).getText().contains("SIGN UP"));
+        Assert.assertTrue(driver.findElement(By.xpath("//html/body/div[1]/section[2]/div[1]/h2")).getText().contains("SIGN UP"));
     }
 
 
     @Test
     public void updatedLoginFields() throws InterruptedException {
-        loginObj = new LoginPage(loginDriver);
+        loginObj = new LoginPage(driver);
         loginObj.clickLoginLink();
         Thread.sleep(2000);
         loginObj.verifyAndAddLoginIinfo();
         loginObj.clickLoginButton();
         Thread.sleep(2000);
-        Assert.assertTrue(loginDriver.findElement(By.xpath("//html/body/div[1]/section/div/form/fieldset/div[1]/p")).getText().contains(invalidLoginMessage));
+        Assert.assertTrue(driver.findElement(By.xpath("//html/body/div[1]/section/div/form/fieldset/div[1]/p")).getText().contains(invalidLoginMessage));
     }
 
-    @AfterSuite
-    public void tearDown() {
-        loginDriver.quit();
-    }
 }
